7ae71d88501be55f762a60725275f278ba4fdb66
Muhamad Faisal <61342260+Icankkkk@users.noreply.github.com>
2022-09-06 01:33:34 +0800

    Add Currency and edit SaveLoadData

---
+++ Assets/Scripts/Global/CurrencyData.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Faisal.Global
+{
+    public class CurrencyData : MonoBehaviour
+    {
+        private const string _prefsKey = "Currency";
+
+        [SerializeField] private int _goldAmount;
+        [SerializeField] private int _addGold = 100;
+
+        public int GoldAmount() { return _goldAmount; }
+
+        public static CurrencyData currencyInstance;
+
+        private void Awake()
+        {
+            if (currencyInstance == null)
+            {
+                currencyInstance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            Load();
+        }
+
+        private void Load()
+        {
+            if (PlayerPrefs.HasKey(_prefsKey))
+            {
+                string json = PlayerPrefs.GetString(_prefsKey);
+                JsonUtility.FromJsonOverwrite(json, this);
+            }
+            else
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            string json = JsonUtility.ToJson(this);
+            PlayerPrefs.SetString(_prefsKey, json);
+        }
+
+        public void AddingPlayerGold()
+        {
+            _goldAmount += _addGold;
+            Save();
+        }
+
+        public void SpendPlayerGold(int price)
+        {
+            _goldAmount -= price;
+            Save();
+        }
+    }
+}
--- Assets/Scripts/Global/SaveLoadData.cs
+++ Assets/Scripts/Global/SaveLoadData.cs
@@ -4,9 +4,61 @@ using UnityEngine;
 
 namespace Faisal.Global
 {
-    public class SaveLoadData : MonoBehaviour
+    public class SaveLoadData : MonoBehaviour 
     {
-        
+        public static SaveLoadData saveLoadInstance;
+        private const string _prefsKey = "SaveData";
+
+        [SerializeField] private int _currentTheme;
+        [SerializeField] private bool[] _isBoughtTheme;
+
+        public int CurrentTheme() { return _currentTheme; }
+        public bool[] IsBoughtTheme() { return _isBoughtTheme; }
+
+        private void Awake()
+        {
+            if (saveLoadInstance == null)
+            {
+                saveLoadInstance = this;
+                DontDestroyOnLoad(gameObject);
+            } else
+            {
+                Destroy(gameObject);
+            }
+            Load();
+        }
+
+        public void Load()
+        {
+            if(PlayerPrefs.HasKey(_prefsKey))
+            {
+                string json = PlayerPrefs.GetString(_prefsKey);
+                JsonUtility.FromJsonOverwrite(json, this);
+            } 
+            else
+            {
+                Save();
+            }
+        }
+
+        public void Save()
+        {
+            string json = JsonUtility.ToJson(this);
+            PlayerPrefs.SetString(_prefsKey, json);
+        }
+
+        public void ChangeTheme(int _themes)
+        {
+            if (_isBoughtTheme[_themes])
+            {
+                _currentTheme = _themes;
+            }
+            else
+            {
+                int _cost = (_themes - 1) * 100;
+            }
+            Save();
+        }
     }
 }
 
