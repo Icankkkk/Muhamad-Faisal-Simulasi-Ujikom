2b74008faeee8dd2ecafab7840b1cb211bd0af1f
Muhamad Faisal <61342260+Icankkkk@users.noreply.github.com>
2022-09-05 10:16:40 +0800

    Create SaveData and EventManager Script

---
+++ Assets/Scripts/Global/SaveLoadData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Faisal.Global
+{
+    public class SaveLoadData : MonoBehaviour
+    {
+        
+    }
+}
+
